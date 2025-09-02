a450a448c646512a797aa22fa67154c9dec95a52
lucasbfaria <lucasbuenodefaria@gmail.com>
2025-09-02 10:23:24 -0300

    atv11

---
+++ atv11/session/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="post" action="verifica.php">
+        <label>Usuário:</label>
+        <input type="text" name="usuario" required><br><br>
+
+        <label>Senha:</label>
+        <input type="password" name="senha" required><br><br>
+
+        <input type="submit" value="Logar">
+    </form>
+</body>
+</html>
