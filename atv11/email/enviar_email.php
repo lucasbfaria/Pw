0d114579c844028523662df70eb2ec11625b31da
lucasbfaria <lucasbuenodefaria@gmail.com>
2025-09-02 09:57:14 -0300

    email

---
+++ atv11/email/enviar_email.php
@@ -0,0 +1,34 @@
+<?php
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+try {
+    $mail->isSMTP();                                    
+    $mail->Host = 'smtp.gmail.com';                     
+    $mail->SMTPAuth = true;                             
+    $mail->Username = 'email.com';            
+    $mail->Password = 'ifba@2029';      
+    $mail->SMTPSecure = 'tls';                         
+    $mail->Port = 587;                                  
+
+    $mail->setFrom('email.com', 'Formulário de Contato');
+    $mail->addAddress($_POST['email']);
+
+    $mail->isHTML(true);
+    $mail->Subject = $_POST['assunto'];
+    $mail->Body    = '<b>Nome:</b> '.$_POST['nome'].'<br><b>Mensagem:</b><br>'.$_POST['mensagem'];
+
+    if($mail->send()){
+        echo 'Mensagem enviada com sucesso!';
+    }
+        echo 'Mensagem não enviada';
+} catch (Exception $e) {
+    echo "Erro ao enviar: {$mail->ErrorInfo}";
+}
+?>
